e2fc64c8e2f350914b409badceecad5e94e933a5
jjaenim <joshuatead@gmail.com>
2023-04-28 20:11:58 +0800

    Lets the user input words

--- Problem 3.py
+++ Problem 3.py
@@ -7,5 +7,7 @@ import random
 def write():
     # open mylife.txt and put the write function to it
     try:
-        with open("C:/assignments_oop/TRYYY/mylife.txt", "w") as my_life:
+        with open("C:/assignments_oop/A4-Problem3/mylife.txt", "w") as my_life:
             while True:
+                 # put a variable to where the user will input their lines
+                line = input("Enter a line (press Enter to exit): ")
