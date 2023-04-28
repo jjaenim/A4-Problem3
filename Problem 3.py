83ca50c0f8823ead49006877a250fbd8243d2f54
jjaenim <joshuatead@gmail.com>
2023-04-28 20:10:51 +0800

    Open mylife.txt and put the write function to it

--- Problem 3.py
+++ Problem 3.py
@@ -2,15 +2,10 @@
 # BSCpE 1-4
 # Assignment 4 - Problem 3
 
-# Pseudocode
-# open mylife.txt and put the write function to it
-# put a variable to where the user will input their lines
-# exit the loop if the user presses Enter
-# to write a new line on the file
-# ask the user if they want to enter another line
-# make a comment
-# do a while loop to ask the user repeatedly
-# to write a new line on the file
-# ask again the user if they want to continue
-# call the function
-# end
+import random
+
+def write():
+    # open mylife.txt and put the write function to it
+    try:
+        with open("C:/assignments_oop/TRYYY/mylife.txt", "w") as my_life:
+            while True:
