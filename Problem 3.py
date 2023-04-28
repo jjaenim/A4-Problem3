a7293cb901d26bd143ea80f6cd3dbd7f8522a0e1
jjaenim <joshuatead@gmail.com>
2023-04-28 20:08:40 +0800

    Pseudocode

--- Problem 3.py
+++ Problem 3.py
@@ -1,3 +1,16 @@
 # Joshua Adrian O. Daet
 # BSCpE 1-4
 # Assignment 4 - Problem 3
+
+# Pseudocode
+# open mylife.txt and put the write function to it
+# put a variable to where the user will input their lines
+# exit the loop if the user presses Enter
+# to write a new line on the file
+# ask the user if they want to enter another line
+# make a comment
+# do a while loop to ask the user repeatedly
+# to write a new line on the file
+# ask again the user if they want to continue
+# call the function
+# end
